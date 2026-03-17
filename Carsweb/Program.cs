2c723599ebaf3d8a1e63dade17bfa936ecf2d794
streaku1 <tery581@gmail.com>
2026-03-17 11:06:15 +0200

    latest

--- Carsweb/Program.cs
+++ Carsweb/Program.cs
@@ -1,3 +1,6 @@
+using Carsweb.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Carsweb
 {
     public class Program
@@ -6,16 +9,16 @@ namespace Carsweb
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -23,7 +26,6 @@ namespace Carsweb
             app.UseStaticFiles();
 
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.MapControllerRoute(
